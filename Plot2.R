433430d3f9cf842abe11f165802a90f6c1461d43
Saif178 <66514227+Saif178@users.noreply.github.com>
2020-06-16 19:15:45 +0530

    Add files via upload

--- Plot2.R
+++ Plot2.R
@@ -1,11 +1,12 @@
 hpc<-read.table('./data/household_power_consumption.txt',header=TRUE,sep=';',
                 'na.strings'='?')
+datetime<-strptime(paste(hpc$Date, hpc$Time, sep=" "),
+                   "%d/%m/%Y %H:%M:%S")
+hpc<-cbind(hpc,datetime)
 hpc$Date<-as.Date(hpc$Date,format='%d/%m/%Y')
-hpc$Time<-hms::as.hms(hpc$Time)
-hpc_subset<-subset(hpc,Date=='2007-02-01')
-hpc_subset_1<-subset(hpc,Date=='2007-02-02')
-hpc_subset<-rbind(hpc_subset,hpc_subset_1)
+hpc$Time<-format(hpc$Time,format='%H:%M:%S')
+hpc_subset<-subset(hpc,Date=='2007-02-01'|Date=='2007-02-02')
 png('plot2.png')
-with(hpc_subset,plot(strptime(Time,'%H:%M:%S'),Global_active_power,
+with(hpc_subset,plot(datetime,Global_active_power,
                      type='l',ylab='Global Active Power (kilowatts)',xlab=''))
 dev.off()
--- Plot3.R
+++ Plot3.R
@@ -1,15 +1,16 @@
 hpc<-read.table('./data/household_power_consumption.txt',header=TRUE,sep=';',
                 'na.strings'='?')
+datetime<-strptime(paste(hpc$Date, hpc$Time, sep=" "),
+                   "%d/%m/%Y %H:%M:%S")
+hpc<-cbind(hpc,datetime)
 hpc$Date<-as.Date(hpc$Date,format='%d/%m/%Y')
-hpc$Time<-hms::as.hms(hpc$Time)
-hpc_subset<-subset(hpc,Date=='2007-02-01')
-hpc_subset_1<-subset(hpc,Date=='2007-02-02')
-hpc_subset<-rbind(hpc_subset,hpc_subset_1)
+hpc$Time<-format(hpc$Time,format='%H:%M:%S')
+hpc_subset<-subset(hpc,Date=='2007-02-01'|Date=='2007-02-02')
 png('Plot3.png')
-with(hpc_subset,plot(strptime(Time,'%H:%M:%S'),Sub_metering_1,
+with(hpc_subset,plot(datetime,Sub_metering_1,
                      type='l',ylab='Energy sub metering',xlab=''))
-lines(strptime(hpc_subset$Time,'%H:%M:%S'),hpc_subset$Sub_metering_2,col='red')
-lines(strptime(hpc_subset$Time,'%H:%M:%S'),hpc_subset$Sub_metering_3,col='blue')
+lines(hpc_subset$datetime,hpc_subset$Sub_metering_2,col='red')
+lines(hpc_subset$datetime,hpc_subset$Sub_metering_3,col='blue')
 legend('topright',col=c('black','red','blue'),lty=c(1,1,1),
        legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'))
 dev.off()
